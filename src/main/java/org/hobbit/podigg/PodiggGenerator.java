efa1ba32db365224fc3e11a093342324284dcfe5
Ruben Taelman <ruben.taelman@ugent.be>
2016-12-16 13:37:46 +0100

    Invoke new podigg-lc js script

--- src/main/java/org/hobbit/podigg/PodiggGenerator.java
+++ src/main/java/org/hobbit/podigg/PodiggGenerator.java
@@ -16,7 +16,8 @@ public class PodiggGenerator extends AbstractDataGenerator {
     protected void generateData() throws Exception {
         // Call the generator
         FileUtils.forceMkdir(new File("output_data"));
-        Process process = Runtime.getRuntime().exec("bin/generate.sh output_data");
+        String memory = System.getenv().getOrDefault("NODE_MEM", "2056");
+        Process process = Runtime.getRuntime().exec("node --harmony_destructuring --max_old_space_size=" + memory + " bin/generate-env output_data");
         process.waitFor();
         System.err.println(IOUtils.toString(process.getErrorStream()));
         System.out.println(IOUtils.toString(process.getInputStream()));
