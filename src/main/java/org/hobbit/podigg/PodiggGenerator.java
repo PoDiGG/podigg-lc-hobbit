ac21d5c77c911054f2aeb28beb1866ff91361984
Ruben Taelman <ruben.taelman@ugent.be>
2017-03-23 09:23:05 +0100

    Improve redirection of logs

--- src/main/java/org/hobbit/podigg/PodiggGenerator.java
+++ src/main/java/org/hobbit/podigg/PodiggGenerator.java
@@ -19,18 +19,21 @@ public class PodiggGenerator extends AbstractComponent {
 
     public static final String DEFAULT_QUEUE_NAME = "hobbit.podigg.queuename";
 
+    protected void generate() throws Exception {
+        // Call the generator
+        FileUtils.forceMkdir(new File("output_data"));
+        String memory = System.getenv().getOrDefault("NODE_MEM", "2056");
+        ProcessBuilder processBuilder = new ProcessBuilder("node", "--max_old_space_size=" + memory, "bin/generate-env", "output_data");
+        processBuilder.inheritIO();
+        processBuilder.start().waitFor();
+    }
+
     @Override
     public void run() throws Exception {
         String queueName = System.getenv().getOrDefault(Constants.DATA_QUEUE_NAME_KEY, DEFAULT_QUEUE_NAME);
         SimpleFileSender sender = SimpleFileSender.create(this, queueName);
 
-        // Call the generator
-        FileUtils.forceMkdir(new File("output_data"));
-        String memory = System.getenv().getOrDefault("NODE_MEM", "2056");
-        Process process = Runtime.getRuntime().exec("node --harmony_destructuring --max_old_space_size=" + memory + " bin/generate-env output_data");
-        process.waitFor();
-        System.err.println(IOUtils.toString(process.getErrorStream()));
-        System.out.println(IOUtils.toString(process.getInputStream()));
+        generate();
 
         // Forward the output
         InputStream is = new FileInputStream("output_data/lc.ttl");
@@ -41,5 +44,9 @@ public class PodiggGenerator extends AbstractComponent {
         } finally {
             IOUtils.closeQuietly(is);
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new PodiggGenerator().generate();
     }
 }
